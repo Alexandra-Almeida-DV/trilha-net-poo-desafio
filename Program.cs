9864057bb1abd3e11ae6e9c12574cd73341b5671
Alexandra Almeida <138154188+Alexandra-Almeida-DV@users.noreply.github.com>
2025-03-10 23:44:58 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
+using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Testando a classe Nokia
+        Nokia nokia = new Nokia("1234-5678", "Nokia 3310", "111111111", 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        // Testando a classe Iphone
+        Iphone iphone = new Iphone("8765-4321", "iPhone 15", "222222222", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
